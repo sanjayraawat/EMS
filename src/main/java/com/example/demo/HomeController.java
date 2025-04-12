af0acb4c7adaac5da430a31e810565b5b344fa1a
abajaj022 <bajaj.amit022@gmail.com>
2025-04-12 15:13:09 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -15,76 +15,108 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.repo.OrganizationRepository;
 import com.example.demo.service.HomeService;
 
 @Controller
 public class HomeController {
+    
+	
+    
+
+    @Autowired
+    private HomeService homeService;
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+ 
+
+    @GetMapping("/user")
+    public String home(Model model) {
+    	List<Organization> organizations = organizationRepository.findAll();
+        model.addAttribute("name", "Spring Boot + Thymeleaf");
+        model.addAttribute("organizations", organizations);
+        return "index"; 
+    }
+
+    @PostMapping("/userCreation")
+    @ResponseBody
+    public Map<String, String> createUser(@ModelAttribute User user ,  @RequestParam("photo") MultipartFile photo) 
+    {
+    	
+        System.out.println("Username: " + user.toString());
+        Optional<User> dbUser = homeService.validateUser(user.getEmail());
+        Map<String, String> response = new HashMap<>();
+
+        if (dbUser.isEmpty()) {
+            homeService.addUser(user,photo);
+            response.put("status", "success");
+            response.put("message", "User created successfully.");
+        } else {
+            response.put("status", "error");
+            response.put("message", "Email already registered.");
+        }
+
+        return response;
 
 
+        
+    }
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        List<User> users = homeService.getAllUsers();
+        model.addAttribute("users", users);
+        return "user_list"; 
+    }
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login"; 
+    }
+    @PostMapping("/checkUser")
+    public  ResponseEntity<?>  validateUser(@ModelAttribute User user, Model model) {
+        System.out.println("user: " + user.toString());
+        Optional<User> dbuser=homeService.validateUser(user.getEmail());
+        System.out.println("db user: " + dbuser.toString());
+        if(user.getPassword().equals(dbuser.get().getPassword())) {
+            List<User> users = homeService.getAllUsers();
+            return ResponseEntity.ok(Map.of( "status", "success","users", users));
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("error", "Invalid password!"));
 
-	@Autowired
-	private HomeService homeService;
-
-
-
-	@GetMapping("/user")
-	public String home(Model model) {
-		model.addAttribute("name", "Spring Boot + Thymeleaf");
-		return "index"; 
-	}
-
-	@PostMapping("/editUser")
-	public  String editUser(@ModelAttribute User user) {
-		System.out.println("Username: " + user.toString());
-		homeService.updateUser(user);
-		return "redirect:/users";
-	}
-
-	@PostMapping("/userCreation")
-	public  ResponseEntity<?> createUser(@ModelAttribute User user) {
-		System.out.println("Username: " + user.toString());
-		Optional<User> dbu1= homeService.validateUser(user.getEmail());
-		if(dbu1.isEmpty()) {
-			homeService.addUser(user);
-			return ResponseEntity.ok(Map.of( "status", "success"));
-		}
-		else {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-					.body(Map.of("status", "error"));
-		}
-	}
-	@GetMapping("/updateUsername/{id}")
+        }
+    }
+    
+    
+    @GetMapping("/updateUsername/{id}")
+    
+    
 	public  String updateUsername(@PathVariable("id") String id, Model model) {
-		System.out.println("id "+id);
+    	 
+    	System.out.println("id "+id);
 		Optional<User> dbuser=homeService.validateUser(id);
 		System.out.println("db user "+dbuser);
 		model.addAttribute("user",dbuser.get());
-		return "edit_user";
-	}
-	@GetMapping("/users")
-	public String getUsers(Model model) {
-		List<User> users = homeService.getAllUsers();
-		model.addAttribute("users", users);
-		return "user_list"; 
-	}
-	@GetMapping("/login")
-	public String login(Model model) {
-		return "login"; 
-	}
-	@PostMapping("/checkUser")
-	public  ResponseEntity<?>  validateUser(@ModelAttribute User user, Model model) {
-		System.out.println("user: " + user.toString());
-		Optional<User> dbuser=homeService.validateUser(user.getEmail());
-		System.out.println("db user: " + dbuser.toString());
-		if(user.getPassword().equals(dbuser.get().getPassword())) {
-			List<User> users = homeService.getAllUsers();
-			return ResponseEntity.ok(Map.of( "status", "success","users", users));
-		}
-		else {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-					.body(Map.of("error", "Invalid password!"));
+       
+           return "edit_user";
 
-		}
+       
+		
 	}
+    
+    @PostMapping("/editUser")
+    public String updateUsername(@RequestParam String email,
+            @RequestParam String username,
+            RedirectAttributes redirectAttributes) {
+
+homeService.changeUsernameByEmail(email, username);
+redirectAttributes.addFlashAttribute("message", "Username updated successfully!");
+return "redirect:/users";
+    }
+
 }
