037035fc840a4eede491f93c7ca85a7718dc8341
abajaj022 <bajaj.amit022@gmail.com>
2025-04-12 15:16:15 +0530

    Update UserRepository.java

--- src/main/java/com/example/demo/repo/UserRepository.java
+++ src/main/java/com/example/demo/repo/UserRepository.java
@@ -6,15 +6,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import jakarta.transaction.Transactional;
+
+
+
+public interface UserRepository extends JpaRepository<User, Long> 
+{
+	
+	Optional<User> findByEmail(String email);
 	@Modifying
 	@Transactional
 	@Query("UPDATE User u SET u.username = :username WHERE u.email = :email")
 	int updateUsernameByEmail(@Param("email") String email, @Param("username") String username);
-
-	Optional<User> findByEmail(String email);
-}
+	
+	}
